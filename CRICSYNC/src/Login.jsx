a5dfe1176c6cd6cd616a7d886f5d8732174e0d71
unknown <ksshivaani18@gmail.com>
2025-06-07 07:37:59 +0530

    feat: added user login

---
+++ CRICSYNC/src/Login.jsx
@@ -0,0 +1,12 @@
+
+function Login() {
+  return (
+        <><h2>USER LOGIN</h2>
+        <input type="text" placeholder="email id or mobile number" required /><br /><br />
+        <input type="text" placeholder="password" required /><br /><br />
+        <label>forgot password?</label><br></br>
+        <button type="submit">Submit</button></>
+  );
+}
+
+export default Login;
