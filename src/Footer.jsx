735239b0f4abe675667c3ba57d0360e29f44e498
SANJIV KUMAR <156926652+SANJIVVV@users.noreply.github.com>
2025-06-11 10:07:54 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -16,43 +16,6 @@ function Footer(){
       <a href="./src/AdminLogin.jsx" className="mt-2 inline-block bg-yellow-400 text-black px-6 py-2 rounded-full font-bold">Login to Admin Panel</a>
     </section>
 
-    <footer className="bg-gray-800 text-gray-100 py-8">
-      <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
-        
-        <div>
-          <h3 className="text-lg font-semibold mb-2">Contact Us</h3>
-          <ul>
-            <li>Email: Yotta@gmail.com</li>
-            <li>Phone: +91 9043872476</li>
-            <li>Address: PSG Tech, Coimbatore</li>
-          </ul>
-        </div>
-
-       
-        <div>
-          <h3 className="text-lg font-semibold mb-2">Privacy</h3>
-          <ul className="space-y-1">
-            <li><a href="#" className="hover:text-blue-400">Privacy Policy</a></li>
-            <li><a href="#" className="hover:text-blue-400">Terms of Service</a></li>
-            <li><a href="#" className="hover:text-blue-400">Cookies</a></li>
-          </ul>
-        </div>
-
-      
-        <div>
-          <h3 className="text-lg font-semibold mb-2">Follow Us</h3>
-          <div className="flex space-x-4">
-            <a href="#" className="hover:text-blue-400">Instagram</a>
-            <a href="#" className="hover:text-blue-400">Facebook</a>
-            <a href="#" className="hover:text-blue-400">Twitter</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="text-center text-xs mt-6 text-gray-500">
-        @2025  Yotta Web Services. All rights reserved.
-      </div>
-    </footer>
     </>
   );
 }
